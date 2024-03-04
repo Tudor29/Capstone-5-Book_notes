ee066febb10c74f3a679110e49b673c90a2a0170
Tudor Birsan <tudorbirsan@Tudors-MacBook-Air.local>
2024-03-04 12:08:50 +0200

    edit and delete buttons are functional

--- index.js
+++ index.js
@@ -110,6 +110,62 @@ app.post("/add-note", async (req, res) => {
   }
 });
 
+app.get("/edit-note/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await db.query("SELECT * FROM books WHERE bood_id = $1", [
+      id,
+    ]);
+    if (result.rows.length > 0) {
+      const note = result.rows[0];
+      res.render("edit", {
+        bood_id: note.bood_id,
+        title: note.title,
+        author: note.author,
+        notes: note.notes,
+        rating: note.rating,
+        coverImgUrl: note.cover_img_id
+          ? `https://covers.openlibrary.org/b/id/${note.cover_img_id}-M.jpg`
+          : "",
+        editMode: true,
+      });
+    } else {
+      res.send("Note not found.");
+    }
+  } catch (error) {
+    console.error("Error fetching note for editing:", error);
+    res.send("Error fetching note for editing.");
+  }
+});
+
+app.post("/update-note/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, notes, rating, author } = req.body;
+
+  try {
+    await db.query(
+      "UPDATE books SET title = $1, author = $2, notes = $3, rating = $4 WHERE bood_id = $5",
+      [title, author, notes, rating, id]
+    );
+
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error updating book note:", error);
+    res.status(500).send("Error updating book note.");
+  }
+});
+
+app.post("/delete-note/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await db.query("DELETE FROM books WHERE bood_id = $1", [id]);
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error deleting book note:", error);
+    res.send("Error deleting book note.");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
